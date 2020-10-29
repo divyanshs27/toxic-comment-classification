7610e4e5dc42f05c4328064afc28f09d198ab564
Divyansh Singhal <54583331+divyanshs27@users.noreply.github.com>
2020-10-30 00:22:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def main():
         </div><br>
         """
         st.markdown(html_temp, unsafe_allow_html=True)
-        image=Image.open('sources/1st.jpeg')
+        image=Image.open('Sources/1st.jpeg')
         st.image(image,use_column_width=True)
         st.title("Problem Statement and Background ")
         st.subheader("Maintaining a decorum on an online forum is the need of the hour. Defined as “willful and repeated harm inflicted through the medium of electronic text”, cyber bullying puts targets under attack from a barrage of degrading, threatening, and/or sexually explicit messages and images conveyed using web sites, instant messaging, blogs, chat rooms, cell phones, web sites, e‐mail, and personal online profiles. Thus, the task of identifying and removing toxic communication from public forums is critical and is infeasible for human moderators.")
@@ -21,16 +21,16 @@ def main():
        
         st.header("The exact problem statement was thus as below:")
         st.subheader("Given a group of sentences or paragraphs, used as a comment by a user in an online platform, classify it to belong to one or more of the following categories — toxic, severe-toxic, obscene, threat, insult or identity-hate with either approximate probabilities or discrete values (0/1). Consider the following examples :")
-        image=Image.open('sources/1.png')
+        image=Image.open('Sources/1.png')
         st.image(image,use_column_width=True)
         st.title("Dataset and EDA")
         st.subheader("The Dataset used for this task is sourced from a Kaggle competition and is titled as the Jigsaw/Conversation AI Toxic Comment Classification Challenge Dataset. The creator have so far built a range of publicly available models served through the Perspective API and created this competition to enable participants to build a multi-headed model that is capable of detecting different types of toxicity like threats, obscenity, insults, and identity based hate better than their models. The dataset is composed of comments from Wikipedia’s talk page edits. The various categorizations for the comments are: toxic, severe toxic, obscene, threat, insult, and identity hate.The training dataset consists of 160k training samples and the test set consists of 153k samples.Understanding the dataset is an extremely vital task and there are several insights to be drawn from the dataset. ")
-        image=Image.open('sources/3rd.png')
+        image=Image.open('Sources/3rd.png')
         st.image(image,use_column_width=True)
         st.subheader("Another interesting data analysis that has been performed is the analysis of the correlation across several parameters shown in the following figures.")
-        image=Image.open('sources/4th.png')
+        image=Image.open('Sources/4th.png')
         st.image(image,use_column_width=True)
-        image=Image.open('sources/5th.png')
+        image=Image.open('Sources/5th.png')
         st.image(image,use_column_width=True)
        
         st.title("Word Frequencies")
@@ -39,12 +39,12 @@ def main():
         st.subheader("- IDF: Downscales words that appear a lot across documents ")
         st.subheader("This highlights those words that are more interesting, e.g. frequent in a document but not across documents. The weight of a term that occurs in a document is simply proportional to the term frequency.")
         pics = {
-                    "Toxic": Image.open('sources/6th.png'),
-                    "Severe_Toxic": Image.open('sources/7th.png'),
-                    "Obscene": Image.open('sources/8th.png'),
-                    "Threat": Image.open('sources/9th.png'),
-                    "Insult": Image.open('sources/10th.png'),
-                    "Identity_hate": Image.open('sources/11th.png'),
+                    "Toxic": Image.open('Sources/6th.png'),
+                    "Severe_Toxic": Image.open('Sources/7th.png'),
+                    "Obscene": Image.open('Sources/8th.png'),
+                    "Threat": Image.open('Sources/9th.png'),
+                    "Insult": Image.open('Sources/10th.png'),
+                    "Identity_hate": Image.open('Sources/11th.png'),
 
                     
                 }
